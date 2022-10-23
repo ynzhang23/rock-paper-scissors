5366a69a9f7ef3434234930f18a90b5ce40439cf
Yining <ynzhang23@gmail.com>
2022-10-23 13:30:53 +0800

    Create function which determines result of the match

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
-getComputerChoice();
-getPlayerSelection();
+let computer = getComputerChoice();
+let player = getPlayerSelection();
+let winner = declareWinner(player, computer);
 
 function getComputerChoice() {
     let computerChoice = Math.floor(Math.random() * 3 + 1);
     switch (computerChoice) {
         case 1:
-            computerChoice = 'Rock';
+            computerChoice = 'rock';
             break;
         case 2:
-            computerChoice = 'Paper';
+            computerChoice = 'paper';
             break;
         case 3:
-            computerChoice = 'Scissors';
+            computerChoice = 'scissors';
             break;        
     }
-    console.log(computerChoice);
+    return computerChoice;
 }
 
 function getPlayerSelection() {
     let playerSelection = prompt("Input your selection: ").toLowerCase();
-    console.log(playerSelection);
     if (playerSelection === "rock" || playerSelection ===  "paper" || playerSelection === "scissors") {
         alert("Entry valid");
         return playerSelection;
-    } else {
-        alert("Entry invalid");
-        getPlayerSelection();
+    } 
+    else {
+        alert("Entry invalid");    
     }
 }
 
-function declareWinner(getPlayerSelection, ) {
-    
+function declareWinner(player, computer) {
+    // If playerSelection is rock
+    let winner;
+    if (player === "rock") {
+        if (computer === "paper") {
+            return winner = "Computer";
+        } else if (computer === "scissors") {
+            return winner = "Player";
+        } else if (computer === "rock") {
+            return winner = "Draw";
+        }
+    }
+    // If playerSelection is paper
+    else if (player === "paper") {
+        if (computer === "paper") {
+            return winner = "Draw";
+        } else if (computer === "scissors") {
+            return winner = "Computer";
+        } else if (computer === "rock") {
+            return winner = "Player";
+        }
+    }
+    // If playerSelection is scissors
+    else if (player === "scissors") {
+        if (computer === "paper") {
+            return winner = "Player";
+        } else if (computer === "scissors") {
+            return winner = "Draw";
+        } else if (computer === "rock") {
+            return winner = "Computer";
+        }
+    }
 }
